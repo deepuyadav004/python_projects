c74db4cdc41c0bae33a2c388c5554db4c65ee977
deepuyadav004 <77964981+deepuyadav004@users.noreply.github.com>
2021-09-15 18:48:18 +0530

    Create madlib.py

---
+++ madlib.py
@@ -0,0 +1,8 @@
+
+adj=input("Adjective")
+verb=input("vert : ")
+verb2=input("verb: ")
+famous_person=input("famous person : ")
+madlib=f"computer programming is so {adj} It makes me so excited all the time because \n I love to {verb}. Stay hydrated and {verb2} like you are {famous_person}"
+print(madlib)
+
